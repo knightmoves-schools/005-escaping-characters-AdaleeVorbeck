4711ead6359c2bb0615117b5a6c9c8a4ff14a4f3
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-02-20 18:36:48 +0000

    IF I RAISE MY VOICE, IF I RAISE MY VOICE, IF I RAISE MY VOICE

--- Email.cs
+++ Email.cs
@@ -2,4 +2,17 @@
 public class Email
 {
     // Add your code here
+    public string introduction = "To whoever may read this, \n" +
+    "\tYou are in grave danger. \n" +
+    "\"Things\" are most likly noticably different \n" +
+    "The \'World\' is different \n \n" +
+    "Goodluck \n" +
+    "Anonomous :\\";
 }
+
+/*should define a class-level string variable named introduction
+should include a new line within introduction
+should include a tab within introduction
+should include a single quote within introduction
+should include a double quote within introduction
+should include a backslash within introduction*/
